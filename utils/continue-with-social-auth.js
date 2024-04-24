26d5de729e0a006578e6445a2bc3a38e2cbdb80c
Jie clark Terec <jieclarkm@gmail.com>
2024-04-25 02:53:38 +0800

    Kadlawon na litse

--- utils/continue-with-social-auth.js
+++ utils/continue-with-social-auth.js
@@ -4,13 +4,20 @@
 import { toast } from "react-toastify";
 export default async function continueWithSocialAuth(provider, redirect) {
   try {
-    const url = `${
-      process.env.NEXT_PUBLIC_HOST
-    }/api/v1/o/${provider}/?redirect_uri=${
+    // const url = `${
+    //   process.env.NEXT_PUBLIC_HOST
+    // }/api/v1/o/${provider}/?redirect_uri=${
+    //   process.env.NODE_ENV === "production"
+    //     ? process.env.NEXT_PUBLIC_REDIRECT_URL
+    //     : "http://localhost:3000"
+    // }/auth/${redirect}`;
+
+    const redirect_uri =
       process.env.NODE_ENV === "production"
         ? process.env.NEXT_PUBLIC_REDIRECT_URL
-        : "http://localhost:3000"
-    }/auth/${redirect}`;
+        : "https://junkfree.netlify.app"; // Fallback for development
+
+    const url = `${process.env.NEXT_PUBLIC_HOST}/api/v1/o/${provider}/?redirect_uri=${redirect_uri}/auth/${redirect}`;
 
     const response = await fetch(url, {
       method: "GET",
