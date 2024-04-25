f5b8cc466331ca7e96946cc5545e54c27e95fdb0
Jie clark Terec <jieclarkm@gmail.com>
2024-04-25 22:05:25 +0800

    balik napod ang suspense

--- app/activation/[uid]/[token]/page.js
+++ app/activation/[uid]/[token]/page.js
@@ -20,7 +20,7 @@ export default function Page({ params }) {
       .finally(() => {
         router.push("/auth/login");
       });
-  }, []);
+  }, [activation, router, params]);
   return (
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
